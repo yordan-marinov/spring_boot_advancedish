6ca05a2395cc440455d3e668f1f46cf9ac44e82f
yordan <yordan@infinitelambda.com>
2022-03-25 16:39:18 +0200

    Created configuration package

--- src/main/java/com/yordanm/spring_boot_advancedish/customer/configurations/CustomerConfig.java
+++ src/main/java/com/yordanm/spring_boot_advancedish/customer/configurations/CustomerConfig.java
@@ -1,2 +1,25 @@
-package com.yordanm.spring_boot_advancedish.customer.configurations;public class CustomerConfig {
+package com.yordanm.spring_boot_advancedish.customer.configurations;
+
+import com.yordanm.spring_boot_advancedish.customer.repositories.CustomerFakeRepo;
+import com.yordanm.spring_boot_advancedish.customer.repositories.CustomerRepo;
+import com.yordanm.spring_boot_advancedish.customer.repositories.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class CustomerConfig {
+
+    @Value("${app.useFakeCustomerRepo:false}")
+    private boolean useFakeCustomerRepo;
+
+    @Bean
+    CustomerRepo customerRepo(){
+        log.info("Customer Fake Repo is: {}", useFakeCustomerRepo);
+        return useFakeCustomerRepo ?
+                new CustomerFakeRepo() :
+                new CustomerRepository();
+    }
 }
