8bda9389b0b8a8988fcce1c5d33f0daf5c974eed
yordan <yordan@infinitelambda.com>
2022-03-25 11:47:55 +0200

    Initial commit

---
+++ src/main/java/com/yordanm/spring_boot_advancedish/SpringBootAdvancedishApplication.java
@@ -0,0 +1,13 @@
+package com.yordanm.spring_boot_advancedish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdvancedishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdvancedishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yordanm/spring_boot_advancedish/SpringBootAdvancedishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yordanm.spring_boot_advancedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdvancedishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
