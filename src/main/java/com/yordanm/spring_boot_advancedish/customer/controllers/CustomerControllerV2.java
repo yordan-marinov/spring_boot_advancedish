2f30f59fc7f819d81cbd68cab5451ae6d31379b2
yordan <yordan@infinitelambda.com>
2022-03-25 19:25:19 +0200

    Added validation for post payload

--- src/main/java/com/yordanm/spring_boot_advancedish/customer/controllers/CustomerControllerV2.java
+++ src/main/java/com/yordanm/spring_boot_advancedish/customer/controllers/CustomerControllerV2.java
@@ -6,6 +6,7 @@ import com.yordanm.spring_boot_advancedish.customer.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,29 +17,30 @@ public class CustomerControllerV2 {
     private final CustomerService customerService;
 
     @GetMapping(path = "/{customerId}")
-    public CustomerDTO returnCustomer(@PathVariable int customerId){
+    public CustomerDTO returnCustomer(@PathVariable int customerId) {
         System.out.println("This is the version 2");
 
         return customerService.getCustomer(customerId);
     }
 
     @GetMapping
-    public List<CustomerDTO> getAll(){
+    public List<CustomerDTO> getAll() {
         System.out.println("This is the version 2");
 
         return customerService.all();
     }
 
     @PostMapping
-    public Customer createCustomer(@RequestBody Customer customer){
-        System.out.println(customer);
+    public CustomerDTO createCustomer(@RequestBody @Valid Customer customer) {
+        final CustomerDTO customerDTO = CustomerDTO.map(customer);
+        System.out.println(customerDTO);
         System.out.println("This is the version 2");
 
-        return customer;
+        return customerDTO;
     }
 
     @DeleteMapping(path = "{customerId}")
-    public CustomerDTO deleteCustomer(@PathVariable Integer customerId){
+    public CustomerDTO deleteCustomer(@PathVariable Integer customerId) {
         CustomerDTO customer = customerService.getCustomer(customerId);
         System.out.println(customer);
         System.out.println("This is the version 2");
