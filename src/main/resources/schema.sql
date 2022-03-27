dd267cf154bdced2d817f807fb56fd31079fb827
yordan <yordan@infinitelambda.com>
2022-03-27 11:50:33 +0300

    Created dummy data and schema for user table in h2 db

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table MOCK_DATA (
+                           id INT,
+                           first_name VARCHAR(50),
+                           last_name VARCHAR(50),
+                           email VARCHAR(50),
+                           password VARCHAR(50)
+);
